c4a1740501a93dec14c36b7c1cc12b8d0c293afa
unknown <ngilbert@PC-CZC046BFNF.netbuilder.private>
2015-06-23 15:12:04 +0100

    gui now launches, need to add functionality

--- src/IMSGUI.java
+++ src/IMSGUI.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 
 import java.awt.Event;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -9,19 +10,34 @@ import java.awt.event.WindowEvent;
 public class IMSGUI extends JFrame {
 	private JFrame mainBox;
 	private JLabel frameLabel;
+	private JLabel statusLabel;
+	private JPanel controlPanel;
 	private void prepareGUI() {
 		mainBox= new JFrame("Inventory Management System");
 		mainBox.setSize(500, 500);
 		mainBox.setLayout(new GridLayout(2,2));
 		frameLabel = new JLabel("",JLabel.CENTER);
+		statusLabel = new JLabel("",JLabel.CENTER);
+		statusLabel.setSize(200,50);
 		mainBox.addWindowListener(new WindowAdapter() {
 			public void
 			windowClose(WindowEvent windowEvent){
 				System.exit(0);
 			}
 		});
+		controlPanel = new JPanel();
+		controlPanel.setLayout(new FlowLayout());
+		mainBox.add(frameLabel);
+		mainBox.add(statusLabel);
+		mainBox.add(controlPanel);
+		mainBox.setVisible(true);
+		
 	}
-	public IMSGUI(){prepareGUI)();}
+	public IMSGUI(){prepareGUI();}
+	
+	private void showEvent() {
+		frameLabel.setText("IMS window view");
+	}
 	
 	public static void main (String args[]){
 		IMSGUI sD = new IMSGUI();
