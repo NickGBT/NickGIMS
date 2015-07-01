096ccf961f029c92c0728e38ba2baac502a3a13d
NickGBT <nick.gilbert@netbuilder.com>
2015-07-01 22:12:12 +0100

    removed unused imports

--- src/Product.java
+++ src/Product.java
@@ -1,6 +1,4 @@
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Product {
 	private String productName;
--- src/StockOrder.java
+++ src/StockOrder.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Properties;
 
 public class StockOrder {
@@ -14,7 +13,6 @@ public class StockOrder {
 		Properties out = new Properties();
 
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
-		Date date = new Date();
 		Calendar rightNow = Calendar.getInstance();
 
 		for (Product p : Product.allProducts) {
